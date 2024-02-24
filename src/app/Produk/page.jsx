a769212b52aa48190a3000bb1e8807e002a3f74c
FajarSant <4jar100so@gmail.com>
2024-02-25 05:46:01 +0700

    Login, register, dan update Api

--- src/app/Produk/page.jsx
+++ src/app/Produk/page.jsx
@@ -1,7 +1,7 @@
 // ProductList.js
 "use client"
 import React, { useEffect, useState } from 'react';
-import ProductCard from './Components/ProdukCard';
+import ProductCard from './Makanan/Components/ProdukCard';
 
 const Product = () => {
   const [products, setProducts] = useState([]);
